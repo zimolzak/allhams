6b791ba3eb4d8aa7e557dccc7402a18ed9c110fe
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 09:23:08 -0600

    Do correct backtick quoting and make scatterplot.

--- repeaters.R
+++ repeaters.R
@@ -7,4 +7,11 @@ message("Reading csv")
 Y = fread(here('texas-2m-repeaters-2022-01-10.csv'))
 head(Y)
 
-qplot(Y$"Output Freq")
+
+
+Y %>% filter(`Output Freq` < 200 & `Input Freq` < 200) -> vhf  # remove cross-band
+cat("--vhf--\n")
+head(vhf)
+
+qplot(vhf$`Output Freq`)
+qplot(vhf$`Input Freq`, vhf$`Output Freq`)
