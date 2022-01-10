eede44d92107b7d339d7610cb091b8cc4a7a7fdb
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 09:39:17 -0600

    Do the 70cm repeaters too.

--- repeaters.R
+++ repeaters.R
@@ -5,6 +5,7 @@ library(ggplot2)
 
 message("Reading csv")
 Y = fread(here('texas-2m-repeaters-2022-01-10.csv'))
+Z = fread(here('texas-70cm-repeaters-2022-01-10.csv'))
 head(Y)
 
 Y %>% filter(`Output Freq` < 200 & `Input Freq` < 200) -> vhf  # remove cross-band
@@ -13,3 +14,10 @@ qplot(vhf$`Input Freq`, vhf$`Output Freq`) +
 	geom_abline(intercept = 0, slope = 1) +
 	scale_x_continuous(minor_breaks=seq(144, 148, 0.2)) +
 	scale_y_continuous(minor_breaks=seq(144, 148, 0.2))
+
+Z %>% filter(`Output Freq` > 400 & `Input Freq` > 400) -> uhf
+qplot(uhf$`Output Freq`)
+qplot(uhf$`Input Freq`, uhf$`Output Freq`) +
+	geom_abline(intercept = 0, slope = 1) +
+	scale_x_continuous(breaks=seq(435, 450, 5)) +
+	scale_y_continuous(breaks=seq(435, 450, 5))
