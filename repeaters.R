b3f23652a18d558a69e405763e45309be119d4b6
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 10:34:56 -0600

    Big reorganization.

--- repeaters.R
+++ repeaters.R
@@ -7,35 +7,46 @@ library(lubridate)
 message("Reading csv")
 Y = fread(here('texas-2m-repeaters-2022-01-10.csv'))
 Z = fread(here('texas-70cm-repeaters-2022-01-10.csv'))
-head(Y)
 
-Y %>% filter(`Output Freq` < 200 & `Input Freq` < 200) -> vhf  # remove cross-band
-qplot(vhf$`Output Freq`)
-ggplot(vhf) + aes(`Output Freq`, `Input Freq`) +
-	geom_jitter(alpha = 0.1) +
-	geom_abline(intercept = 0, slope = 1) +
-	scale_x_continuous(minor_breaks=seq(144, 148, 0.2)) +
-	scale_y_continuous(minor_breaks=seq(144, 148, 0.2))
+Y %>%
+filter(`Output Freq` < 200 & `Input Freq` < 200) %>%
+select(-EchoLink) -> vhf  # remove cross-band
 
-Z %>% filter(`Output Freq` > 400 & `Input Freq` > 400) -> uhf
-qplot(uhf$`Output Freq`)
-ggplot(uhf) + aes(`Output Freq`, `Input Freq`) +
-	geom_jitter(alpha = 0.1) +
-	geom_abline(intercept = 0, slope = 1) +
-	scale_x_continuous(breaks=seq(435, 450, 5)) +
-	scale_y_continuous(breaks=seq(435, 450, 5)) +
-	geom_density_2d()
+Z %>%
+filter(`Output Freq` > 400 & `Input Freq` > 400) %>%
+select(-EchoLink) -> uhf
 
-vhf %>% select(`Uplink Tone`, `Downlink Tone`, Lat, Long, `Last Update`, `Output Freq`) -> vcompare
-uhf %>% select(`Uplink Tone`, `Downlink Tone`, Lat, Long, `Last Update`, `Output Freq`) -> ucompare
-
-bind_rows(vcompare, ucompare, .id = "table.id") %>%
+bind_rows(vhf, uhf, .id = "table.id") %>%
 mutate(up.tone.num = as.numeric(`Uplink Tone`),
 	down.tone.num = as.numeric(`Downlink Tone`),
 	band = case_when(table.id == 1 ~ '2m', table.id == 2 ~ '70cm'),
 	update.dt = as_date(`Last Update`)
 ) %>%
 filter(Long < 0 & Lat < 40) -> compare
+
+head(compare)
+
+
+
+
+###### plots
+
+qplot(vhf$`Output Freq`)
+
+ggplot(vhf) + aes(`Output Freq`, `Input Freq`) +
+	geom_jitter(alpha = 0.1) +
+	geom_abline(intercept = 0, slope = 1) +
+	scale_x_continuous(minor_breaks=seq(144, 148, 0.2)) +
+	scale_y_continuous(minor_breaks=seq(144, 148, 0.2))
+
+qplot(uhf$`Output Freq`)
+
+ggplot(uhf) + aes(`Output Freq`, `Input Freq`) +
+	geom_jitter(alpha = 0.1) +
+	geom_abline(intercept = 0, slope = 1) +
+	scale_x_continuous(breaks=seq(435, 450, 5)) +
+	scale_y_continuous(breaks=seq(435, 450, 5)) +
+	geom_density_2d()
 
 ggplot(compare) +
 	aes(x = up.tone.num, y = down.tone.num, color = band) +
@@ -52,3 +63,7 @@ ggplot(compare) +
 ggplot(compare) +
 	aes(x = update.dt, fill = band) +
 	geom_histogram()
+
+L = fread(here('my_lat_long.csv'))  # just 1 line like 31.234,-90.123
+LAT = L$V1
+LON = L$V2
