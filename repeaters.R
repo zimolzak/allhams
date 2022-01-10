092455785032fbe8849ceed44897988382ffeed2
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 09:32:51 -0600

    Grid that highlights the 0.6 offset.

--- repeaters.R
+++ repeaters.R
@@ -7,11 +7,9 @@ message("Reading csv")
 Y = fread(here('texas-2m-repeaters-2022-01-10.csv'))
 head(Y)
 
-
-
 Y %>% filter(`Output Freq` < 200 & `Input Freq` < 200) -> vhf  # remove cross-band
-cat("--vhf--\n")
-head(vhf)
-
 qplot(vhf$`Output Freq`)
-qplot(vhf$`Input Freq`, vhf$`Output Freq`)
+qplot(vhf$`Input Freq`, vhf$`Output Freq`) +
+	geom_abline(intercept = 0, slope = 1) +
+	scale_x_continuous(minor_breaks=seq(144, 148, 0.2)) +
+	scale_y_continuous(minor_breaks=seq(144, 148, 0.2))
