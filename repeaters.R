39a15c325fb923bbada85944549cae0ea83c233f
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 10:08:20 -0600

    More comparison plots: PL tone density, and date.

--- repeaters.R
+++ repeaters.R
@@ -2,6 +2,7 @@ library(here)
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(lubridate)
 
 message("Reading csv")
 Y = fread(here('texas-2m-repeaters-2022-01-10.csv'))
@@ -24,14 +25,27 @@ qplot(uhf$`Input Freq`, uhf$`Output Freq`) +
 
 vhf %>% select(`Uplink Tone`, `Downlink Tone`, Lat, Long, `Last Update`, `Output Freq`) -> vcompare
 uhf %>% select(`Uplink Tone`, `Downlink Tone`, Lat, Long, `Last Update`, `Output Freq`) -> ucompare
-bind_rows(vcompare, ucompare, .id = "table.id") %>%
-	mutate(up.tone.num = as.numeric(`Uplink Tone`),
-		down.tone.num = as.numeric(`Downlink Tone`),
-		band = case_when(table.id == 1 ~ '2m', table.id == 2 ~ '70cm')
-	) -> compare
 
-head(compare)
+bind_rows(vcompare, ucompare, .id = "table.id") %>%
+mutate(up.tone.num = as.numeric(`Uplink Tone`),
+	down.tone.num = as.numeric(`Downlink Tone`),
+	band = case_when(table.id == 1 ~ '2m', table.id == 2 ~ '70cm'),
+	update.dt = as_date(`Last Update`)
+) %>%
+filter(Long < 0 & Lat < 40) -> compare
 
 ggplot(compare) +
 	aes(x = up.tone.num, y = down.tone.num, color = band) +
-	geom_jitter()
+	geom_jitter(alpha = 0.5)
+
+ggplot(compare) +
+	aes(x = up.tone.num, color = band) +
+	geom_density()
+
+ggplot(compare) +
+	aes(x = Long, y = Lat, color = band) +
+	geom_jitter(alpha = 0.5)
+
+ggplot(compare) +
+	aes(x = update.dt, fill = band) +
+	geom_histogram()
