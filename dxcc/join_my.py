c58630ee0a18d0b5e50b12b68e05c827d7363b97
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2023-12-06 12:42:12 -0600

    honduras is 78

--- dxcc/join_my.py
+++ dxcc/join_my.py
@@ -1,28 +1,34 @@
 import pandas as pd
 
+"""Takes in a text file copy pasted from lotw and hopefully makes it
+easy to say who's new. Not run by makefile. Just do it at command
+line: `python join_my.py`
+
+"""
+
 # Inputs
 
-mine = pd.read_table('my_dxcc.csv', sep=',')
+TABLE_OF_WORKED_DXCC_NUMBERS = pd.read_table('my_dxcc.csv', sep=',')
 dxcc_details = pd.read_table('dxcc.csv', sep=',').rename(columns = {'dxcc_code':'dxcc'})
 
 # read from copy paste file
 
-updated_copy = []
+NEW_LOTW_COUNTRY_LIST = []
 with open('my_from_lotw.txt') as fh:
     for line in fh:
         country_words = line.split()[:-1]
         country_name = ' '.join(country_words) + ' **'
-        updated_copy.append(country_name.upper())
+        NEW_LOTW_COUNTRY_LIST.append(country_name.upper())
 
 # Processing
 
-details_of_worked = pd.merge(mine, dxcc_details, how='left', on=['dxcc'])
+details_of_worked = pd.merge(TABLE_OF_WORKED_DXCC_NUMBERS, dxcc_details, how='left', on=['dxcc'])
 details_of_worked['e with n'] = details_of_worked.entity + details_of_worked.dxcc.apply(lambda n: ' ' + str(n))
 
-in_csv = list(details_of_worked['e with n'])
-in_csv = [s.upper() for s in in_csv]
+OLD_CSV_COUNTRY_LIST = list(details_of_worked['e with n'])
+OLD_CSV_COUNTRY_LIST = [s.upper() for s in OLD_CSV_COUNTRY_LIST]
 
-both_lists = in_csv + updated_copy
+both_lists = OLD_CSV_COUNTRY_LIST + NEW_LOTW_COUNTRY_LIST
 both_lists.sort()
 
 # print
@@ -32,3 +38,11 @@ for s in both_lists:
         print(s, end=' ')
     else:
         print(s)
+
+print("""
+
+** means it came from lotw.
+Number means it's from my_dxcc.csv on disk.
+Basically look for any lines with two **.
+But it gets messed up because alphabetical order and abbreviations.
+""")
